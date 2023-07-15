bbae11d79d041b2ed0231cfa4af9efad8297dfa3
Ammar1122s <you@example.com>
2023-07-15 06:37:05 +0500

    new

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import imutils as imutils
+import numpy as np
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.screen import Screen
@@ -8,6 +9,7 @@ from kivy.lang import Builder
 from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
+from collections import deque
 import cv2
 import time
 
@@ -19,6 +21,11 @@ changeInDistance = 0
 
 listDistance = []
 listSpeed = []
+
+known_distance = 5.0  # Example: 10 feet
+
+# Known ball size in pixels (adjust according to your scenario)
+known_ball_size = 30.0  # Example: 30 pixels
 
 # This is the distance from camera to face object
 DECLARED_LEN = 30 # cm
@@ -32,6 +39,8 @@ WHITE = (255, 255, 255)
 fonts = cv2.FONT_HERSHEY_COMPLEX
 # calling the haarcascade_frontalface_default.xml module for face detection.
 face_detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+averageSpeed1=0
 
 Window.size = (360, 640)
 
@@ -166,6 +175,7 @@ ScreenManager:
         name:"Live"
         MDBoxLayout:
             orientation:"vertical"
+            spacing: dp(10)
             MDTopAppBar:
                 title: 'Live Camera'
                 pos_hint: {'top': 1}
@@ -173,25 +183,76 @@ ScreenManager:
                 anchor_title: 'left'
                 left_action_items: [['arrow-left', lambda x: setattr(screen_manager, 'current', 'home')]]
                 elevation: 1 
+            MDFlatButton:
+                text: 'Check the Speed'
+                theme_text_color: 'Custom'
+                text_color: (1, 1, 1, 1)
+                font_size: '30dp'
+                font_name: 'fonts/mesquin-italic'
+                md_bg_color: (255 / 255, 133 / 255, 51 / 255, 1)
+                size_hint: (0.8, 0.09)
+                pos_hint: {'center_x': 0.4,}
+                on_release: 
+                    app.check_speed()
+            
+            MDFlatButton:
+                text: 'Clear Speed'
+                theme_text_color: 'Custom'
+                text_color: (1, 1, 1, 1)
+                font_size: '30dp'
+                font_name: 'fonts/mesquin-italic'
+                md_bg_color: (255 / 255, 133 / 255, 51 / 255, 1)
+                size_hint: (0.8, 0.09)
+                pos_hint: {'center_x': 0.4,}
+                on_release: 
+                    app.clear()
+                    
+                    
             Image:
                 id: image_widget
+            MDLabel:
+                id:label_id
+                text:""
+                size_hint: (0.5, 0.6)
+                pos_hint: {'center_x': 0.5}
             
                                             
 """
 
 class Velo(MDApp):
 
+
     def build(self):
         global screen
         screen = Builder.load_string(screen_helper)
         self.initialTime = 0
         self.initialDistance = 0
+        self.previous_center = None
+        self.previous_time = time.time()
+        self.averageSpeed = 0.0
+        self.check = False
         return screen
     def on_switch_active(self, active):
         if active:
             print("Switch is ON")
         else:
             print("Switch is OFF")
+
+    def check_speed(self):
+        num = round(self.averageSpeed, 4)
+        label = screen.ids.label_id
+        label.text = "Speed in mph is "+str(num)
+        self.averageSpeed = 0
+        listSpeed.clear()
+
+    def clear(self):
+        self.averageSpeed = 0
+        listSpeed.clear()
+        label = screen.ids.label_id
+        label.text = "Speed in mph is 0.00"
+
+
+
 
     def live_camera(self):
         # layout = MDBoxLayout(orientation="vertical")
@@ -201,8 +262,7 @@ class Velo(MDApp):
 
         self.image = screen.ids.image_widget
         self.capture = cv2.VideoCapture(0)
-        Clock.schedule_interval(self.load_video, 1.0/30.0)
-
+        Clock.schedule_interval(self.load_video, 1.0/20.0)
 
 
     def load_video(self, *args):
@@ -210,62 +270,68 @@ class Velo(MDApp):
         ret, frames = self.capture.read()
         self.image_frame = frames
 
+        ball_width = 7.6
+
+        blurred = cv2.GaussianBlur(frames, (11,11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+        # sensitivity = 15
+        # lower_white = np.array([0,0,255-sensitivity])
+        # upper_white = np.array([255,sensitivity,255])
+
+        lower_white = (29, 86, 6)
+        upper_white = (64, 255, 255)
+        pts = deque(maxlen=64)
+
+        mask = cv2.inRange(hsv, lower_white, upper_white)
+        mask = cv2.erode(mask, None, iterations=3)
+        mask = cv2.dilate(mask, None, iterations=3)
+
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        center = None
 
 
+        if len(cnts) > 0:
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
 
-        # face_width_in_frame = self.face_data()
-        # focal_length_found = self.focal_length(DECLARED_LEN, DECLARED_WID, 1524)
-        # if face_width_in_frame != 0:
-        #     Distance = self.distance_finder(focal_length_found, DECLARED_WID, face_width_in_frame)
-        #     listDistance.append(Distance)
-        #     averageDistance = self.averageFinder(listDistance, 2)
-        #
-        # distanceInMeters = averageDistance/100
-        #
-        # changeInTime =0.1
-        # changeInDistance=0.1
-        #
-        #
-        # if self.initialDistance != 0:
-        #     # getting the  difference of the distances
-        #     changeInDistance = distanceInMeters - self.initialDistance
-        #     changeInTime = time.time() - self.initialTime
-        #
-        # speed = self.speedFinder(coveredDistance=changeInDistance, timeTaken=changeInTime)
-        # listSpeed.append(speed)
-        #
-        # averageSpeed = self.averageFinder(listSpeed, 10)
-        # if averageSpeed < 0:
-        #         averageSpeed = averageSpeed * -1
-        #
-        # speedFill = int(45+(averageSpeed) * 130)
-        # if speedFill > 235:
-        #     speedFill = 235
-        # cv2.line(frames, (45, 70), (235, 70), (0, 255, 0), 35)
-        # cv2.line(frames, (45, 70), (speedFill, 70), (255, 255, 0), 32)
-        # cv2.line(frames, (45, 70), (235, 70), (0, 0, 0), 22)
-        # cv2.putText(frames, f"Speed: {round(averageSpeed*2.23694, 2)} mph", (50, 75), fonts, 0.6, (0, 255, 220), 2)
-        #
-        # self.initialTime = time.time()
-        # self.initialDistance = distanceInMeters
-        #
-        # cv2.line(frames, (45, 25), (255, 25), (255, 0, 255), 30)
-        # cv2.line(frames, (45, 25), (255, 25), (0, 0, 0), 22)
-        # cv2.putText(
-        #     frames, f"Distance = {round(distanceInMeters/1609,4)} m", (50, 30), fonts, 0.6, WHITE, 2)
+            if radius > 10:
+                cv2.circle(frames, (int(x), int(y)), int(radius),
+                           (0, 255, 255), 4)
+                cv2.circle(frames, center, 5, (0, 0, 255), -1)
+
+            if self.previous_center is not None:
+                displacement = ((cx - self.previous_center[0]) ** 2 + (cy - self.previous_center[1]) ** 2) ** 0.5
+                time_elapsed = time.time() - self.previous_time
+
+                speed = (displacement * 0.000008) / (time_elapsed / 3600)
+
+                listSpeed.append(speed)
+                self.averageSpeed = self.averageFinder(listSpeed, 10)
 
 
-        # blurred = cv2.GaussianBlur(frames, (11,11), 0)
-        # hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-        # colorLower = (29, 86, 6)
-        # colorUpper = (64, 255, 255)
-        #
-        # mask = cv2.inRange(hsv, colorLower, colorUpper)
-        # mask = cv2.erode(mask, None, iterations=3)
-        # mask = cv2.dilate(mask, None, iterations=3)
-        #
-        # cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        # cnts = imutils.grab_contours(cnts)
+            self.previous_center = (cx, cy)
+            self.previous_time = time.time()
+
+
+        # if (len(cnts) == 0):
+        #     cv2.line(frames, (45, 70), (235, 70), (0, 255, 0), 35)
+        #     cv2.line(frames, (45, 70), (235, 70), (255, 255, 0), 32)
+        #     cv2.line(frames, (45, 70), (235, 70), (0, 0, 0), 22)
+        #     cv2.putText(frames, f"Speed: {round(self.averageSpeed, 2)} mph", (50, 75), fonts, 0.6, (0, 255, 220), 2)
+
+
+        pts.appendleft(center)
+
+        for i in range(1, len(pts)):
+            if pts[i - 1] is None or pts[i] is None:
+                continue
+            thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
+            cv2.line(frames, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
 
         buffer = cv2.flip(frames, 0).tobytes()
@@ -273,23 +339,8 @@ class Velo(MDApp):
         texture.blit_buffer(buffer, colorfmt='bgr', bufferfmt='ubyte')
         self.image.texture = texture
 
-    def face_data(self, *args):
-        face_width = 0
-        gray_image = cv2.cvtColor(self.image_frame, cv2.COLOR_BGR2GRAY)
-        #We use 1.3 for less powerful processors but can increase it according to your processing power of your machine.
-        faces = face_detector.detectMultiScale(gray_image, 1.3, 5)
-        #getting the rectangular frame
-        for (x, y, h, w) in faces:
-            cv2.rectangle(self.image_frame, (x, y), (x + w, y + h), RED, 3)
-            face_width = w
-        return face_width
 
-    def focal_length(self, determined_distance, actual_width, width_in_rf_image):
-        focal_length_value = (width_in_rf_image * determined_distance) / actual_width
-        return focal_length_value
-    def distance_finder(self, focal_length, real_face_width, face_width_in_frame):
-        distance = (real_face_width * focal_length) / face_width_in_frame
-        return distance
+
 
     def averageFinder(self, completeList, averageOfItems):
         lengthOfList = len(completeList)
@@ -297,13 +348,5 @@ class Velo(MDApp):
         selectedItemsList = completeList[selectedItems:]
         average = sum(selectedItemsList) / len(selectedItemsList)
         return average
-
-    def speedFinder(self,coveredDistance, timeTaken):
-
-        speed = coveredDistance / timeTaken
-
-        return speed
-
-
 
 Velo().run()
